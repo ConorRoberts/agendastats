f2cd5447a014eea080a3df7f55a8df7e6fb3cebb
Conor Roberts <62777912+ConorRoberts@users.noreply.github.com>
2022-02-23 12:28:04 -0500

    Starting to work on contribution scores

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import { Input } from "@components/form";
+import { Button, Input, Select } from "@components/form";
 import { Assault, Medic, Recon, Robotics } from "@components/Icons";
+import PlayerStats from "@typedefs/PlayerStats";
 import getPgClient from "@utils/getPgClient";
 import Link from "next/link";
 import { useState } from "react";
@@ -11,8 +12,9 @@ const icons = {
   recon: <Recon className="w-6 h-6" />
 };
 
-const Page = ({ averages, players, topPlayers }) => {
+const Page = ({ averages, players, topPlayers, contributionScores }) => {
   const [playerQuery, setPlayerQuery] = useState("");
+  const [contributorField, setContributorField] = useState("damage");
   return (
     <div className="flex flex-col gap-8 p-2 mx-auto max-w-5xl w-full">
       <div className="flex flex-col gap-4">
@@ -29,6 +31,45 @@ const Page = ({ averages, players, topPlayers }) => {
             </div>
           ))}
         </div>
+        <h2>Highest Contributors</h2>
+        <div className="flex flex-col sm:flex-row gap-4 sm:items-center">
+          <div className="flex-1">
+            <h4 className="capitalize">{contributorField}</h4>
+            <p>
+              Average % of {contributorField.toLowerCase()} contributed per
+              match (across all matches)
+            </p>
+          </div>
+          <div className="flex capitalize gap-4">
+            {["damage", "healing", "kills"].map((e) => (
+              <div
+                onClick={() => setContributorField(e)}
+                key={`contrib-option-${e}`}
+                className={`rounded-full border-gray-300 border py-1 px-3 ${
+                  contributorField === e && "bg-indigo-500 text-white text-center"
+                }`}
+              >
+                {e}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-0.5 capitalize">
+          {contributionScores
+            .sort((a, b) => b[contributorField] - a[contributorField])
+            .slice(0, 10)
+            .map((player: PlayerStats) => (
+              <div
+                key={`contribution-${player.player_name}-${player[contributorField]}`}
+                className="grid grid-cols-3"
+              >
+                <p>{player.player_name}</p>
+                <p>{player.player_class}</p>
+                <p>{parseFloat(player[contributorField]).toFixed(2)}%</p>
+              </div>
+            ))}
+        </div>
+
         <h2 className="text-center">Top Stats</h2>
         <div className="flex gap-4 flex-col">
           {averages.map(([label, data]) => (
@@ -127,32 +168,35 @@ export const getServerSideProps = async () => {
     await client.query(`SELECT A.player_name,A.player_class,(100-AVG((KILLS+DAMAGE+bot_kills+ABSORBED+HEALING+ASSISTS+BUFFS+obj_pts+DEFENSE)/7.5)) as rank FROM (select player_name,player_class,rank() OVER (PARTITION BY player_CLASS order by sum(KILLS) DESC) KILLS,
       rank() OVER (PARTITION BY player_CLASS order by sum(DAMAGE) DESC) DAMAGE,rank() OVER (PARTITION BY player_CLASS order by sum(bot_kills) DESC) bot_kills,rank() OVER (PARTITION BY player_CLASS order by sum(absorbed) DESC) ABSORBED,rank() OVER (PARTITION BY player_CLASS order by sum(healing) DESC) HEALING,rank() OVER (PARTITION BY player_CLASS order by sum(assists) DESC) ASSISTS,rank() OVER (PARTITION BY player_CLASS order by sum(buffs) DESC) BUFFS,rank() OVER (PARTITION BY player_CLASS order by sum(obj_pts) DESC) obj_pts, rank() OVER (PARTITION BY player_CLASS order by sum(defense) DESC) DEFENSE from stats group by PLAYER_NAME, PLAYER_CLASS )A group by A.PLAYER_NAME, A.PLAYER_CLASS order by rank desc limit 10`);
 
+  const contributionScores = await client.query(
+    "select pc as player_class, pn as player_name,avg(damage) as damage,avg(healing) as healing, avg(kills) as kills,avg(absorbed) as absorbed, avg(defense) as defense, avg(bot_kills) as bot_kills, avg(deaths) as deaths from( select st.player_name as pn,st.player_class as pc, st.match_id as mid, case when (a.TDAMAGE = 0 or st.damage = 0) then 0 else CAST((damage / CAST(TDAMAGE AS DECIMAL(10, 2))) * 100 AS DECIMAL(10, 2)) end as damage, case when (a.TKILLS = 0 or st.kills = 0) then 0 else CAST((kills / CAST(TKILLS AS DECIMAL(10, 2))) * 100 AS DECIMAL(10, 2)) end as kills, case when (a.TASSISTS = 0 or st.assists = 0) then 0 else CAST((assists / CAST(TASSISTS AS DECIMAL(10, 2))) * 100 AS DECIMAL(10, 2)) end as assists, case when (a.TBOTKILLS = 0 or st.bot_kills = 0) then 0 else CAST((bot_kills / CAST(TBOTKILLS AS DECIMAL(10, 2))) * 100 AS DECIMAL(10, 2)) end as bot_kills, case when (a.TDEATHS = 0 or st.deaths = 0) then 0 else CAST((deaths / CAST(TDEATHS AS DECIMAL(10, 2))) * 100 AS DECIMAL(10, 2)) end as deaths, case when (a.TABSORBED = 0 or st.absorbed = 0) then 0 else CAST((absorbed / CAST(TABSORBED AS DECIMAL(10, 2))) * 100 AS DECIMAL(10, 2)) end as absorbed, case when (a.TDEFENSE = 0 or st.defense = 0) then 0 else CAST((defense / CAST(TDEFENSE AS DECIMAL(10, 2))) * 100 AS DECIMAL(10, 2)) end as defense, case when (a.THEALING = 0 or st.healing = 0) then 0 else CAST((healing / CAST(THEALING AS DECIMAL(10, 2))) * 100 AS DECIMAL(10, 2)) end as healing from stats st inner join ( select match_id, sum(kills) TKILLS, sum(damage) TDAMAGE, sum(absorbed) TABSORBED, sum(assists) TASSISTS, sum(bot_kills) TBOTKILLS, sum(deaths) TDEATHS, sum(healing) THEALING, sum(defense) TDEFENSE from STATS group by match_id) a on a.match_id = st.match_id) b group by pn,pc;"
+  );
+
+  const gamesPlayed = await client.query(
+    "select player_name,player_class,count(*) as games_played from stats group by player_name,player_class;"
+  );
   await client.end();
 
   return {
     props: {
       players: players.rows.map((e) => e.player_name),
       averages: [
-        [
-          "Average Damage",
-          highestAverageDmg.rows
-        ],
-        [
-          "Average Kills",
-          highestAverageKills.rows
-        ],
-        [
-          "Average Healing",
-          highestAverageHealing.rows
-        ]
+        ["Average Damage", highestAverageDmg.rows],
+        ["Average Kills", highestAverageKills.rows],
+        ["Average Healing", highestAverageHealing.rows]
       ],
-      singleMatchRecords: [
-        [
-          "Single Match Healing",
-          singleMatchHealing.rows
-        ]
-      ],
-      topPlayers: topPlayers.rows
+      singleMatchRecords: [["Single Match Healing", singleMatchHealing.rows]],
+      topPlayers: topPlayers.rows,
+      contributionScores: contributionScores.rows
+        .map((e) => ({
+          ...e,
+          games_played: gamesPlayed.rows.find(
+            (f) =>
+              f.player_name === e.player_name &&
+              f.player_class === e.player_class
+          ).games_played
+        }))
+        .filter((e) => e.games_played >= 5)
     }
   };
 };
